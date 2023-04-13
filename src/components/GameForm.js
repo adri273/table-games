d78475fdc3743d60e08fa1d5e1d6df33cd23d372
adri273 <adriavellanedamartinez@gmail.com>
2023-04-13 19:38:52 +0200

    Update GameForm.js

--- src/components/GameForm.js
+++ src/components/GameForm.js
@@ -1,45 +1,49 @@
-import React, { useState } from 'react';
-import firebase from '../utils/firebase';
+import React, { useState } from "react";
 
-const GameForm = () => {
-  const [name, setName] = useState('');
-  const [image, setImage] = useState('');
-  const [players, setPlayers] = useState('');
+const GameForm = ({ onSave }) => {
+  const [name, setName] = useState("");
+  const [players, setPlayers] = useState("");
+  const [image, setImage] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    firebase.firestore().collection('games').add({
-      name,
-      image,
-      players,
-    });
-    setName('');
-    setImage('');
-    setPlayers('');
+    onSave({ name, players, image });
+    setName("");
+    setPlayers("");
+    setImage("");
   };
 
   return (
-    <div>
-      <h1>Add Game</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Name:
-          <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
-        </label>
-        <br />
-        <label>
-          Image URL:
-          <input type="text" value={image} onChange={(e) => setImage(e.target.value)} />
-        </label>
-        <br />
-        <label>
-          Players:
-          <input type="text" value={players} onChange={(e) => setPlayers(e.target.value)} />
-        </label>
-        <br />
-        <button type="submit">Add Game</button>
-      </form>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="name">Name:</label>
+      <input
+        type="text"
+        id="name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        required
+      />
+
+      <label htmlFor="players">Number of Players:</label>
+      <input
+        type="number"
+        id="players"
+        value={players}
+        onChange={(e) => setPlayers(e.target.value)}
+        required
+      />
+
+      <label htmlFor="image">Image URL:</label>
+      <input
+        type="url"
+        id="image"
+        value={image}
+        onChange={(e) => setImage(e.target.value)}
+        required
+      />
+
+      <button type="submit">Add Game</button>
+    </form>
   );
 };
 
