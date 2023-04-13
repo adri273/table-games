2d1e3afcf8443de1d82bb4cda0732dea0f223b57
adri273 <adriavellanedamartinez@gmail.com>
2023-04-13 19:18:46 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import { BrowserRouter as Router } from 'react-router-dom';
-import Routes from './routes/Routes';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <Router>
-      <Routes />
-    </Router>
-  );
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import GameList from './components/GameList';
+import GameDetail from './components/GameDetail';
+import Login from './components/Login';
+
+import { AuthProvider } from './contexts/AuthContext';
+import { GameProvider } from './contexts/GameContext';
+
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <AuthProvider>
+          <GameProvider>
+            <div className="App">
+              <Navbar />
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/games" component={GameList} />
+                <Route path="/game/:id" component={GameDetail} />
+                <Route path="/login" component={Login} />
+              </Switch>
+            </div>
+          </GameProvider>
+        </AuthProvider>
+      </Router>
+    );
+  }
 }
 
 export default App;
