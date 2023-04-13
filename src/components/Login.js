326dbfa73ccf7b5f56a6f821c5e991f1a4b82330
adri273 <adriavellanedamartinez@gmail.com>
2023-04-13 19:37:25 +0200

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,40 +1,50 @@
-import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
-import firebase from '../utils/firebase';
+import { useContext, useState } from "react";
+import { Link } from "react-router-dom";
+import { FirebaseContext } from "../contexts/FirebaseContext";
 
 const Login = () => {
-  const history = useHistory();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const { firebase } = useContext(FirebaseContext);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
 
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
       await firebase.auth().signInWithEmailAndPassword(email, password);
-      history.push('/');
-    } catch (error) {
-      setError('Error logging in with email and password');
+    } catch (err) {
+      setError(err.message);
     }
   };
 
   return (
     <div>
-      <h2>Login</h2>
+      <h1>Login</h1>
       <form onSubmit={handleLogin}>
-        <label>
-          Email:
-          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-        </label>
-        <br />
-        <label>
-          Password:
-          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-        </label>
-        <br />
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        {error && <div>{error}</div>}
         <button type="submit">Login</button>
       </form>
-      {error && <p>{error}</p>}
+      <p>
+        Don't have an account? <Link to="/register">Register here</Link>
+      </p>
     </div>
   );
 };
