2c4ed721dc8edb30413367153380867f23282be6
adri273 <adriavellanedamartinez@gmail.com>
2023-04-13 19:10:48 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,56 +1,11 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import Home from './components/Home';
-import GameList from './components/GameList';
-import AddGame from './components/AddGame';
-import Login from './components/Login';
-import Register from './components/Register';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Routes from './routes/Routes';
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/games">Game List</Link>
-            </li>
-            <li>
-              <Link to="/add">Add Game</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-            <li>
-              <Link to="/register">Register</Link>
-            </li>
-            <li>
-              <Logout />
-            </li>
-          </ul>
-        </nav>
-        <Switch>
-          <Route path="/games">
-            <GameList />
-          </Route>
-          <Route path="/add">
-            <AddGame />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/register">
-            <Register />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
+    <Router>
+      <Routes />
+    </Router>
   );
 }
 
